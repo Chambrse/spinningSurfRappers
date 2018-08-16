4601e5083364ad9aed955e6972c63f17802233f3
Chambrse <shaneechambry@gmail.com>
2018-08-16 10:06:40 -0700

    added array of tweets support, can now get any handle

--- routes/index.js
+++ routes/index.js
@@ -11,14 +11,10 @@ var request = require('request');
 var indico = require('indico.io');
 indico.apiKey = keys.indico.api_key;
 
-var text;
-var line = "-------------------------";
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('underConstruction');
 });
-
 
 // Test the sequelize mysql connection. Should display json of the test database items.
 router.get('/test', function (req, res, next) {
@@ -28,35 +24,35 @@ router.get('/test', function (req, res, next) {
   });
 });
 
-router.get('/sentimentTest', function (req, res, next) {
-  var params = { screen_name: 'realDonaldTrump', tweet_mode: 'extended', count: 10 };
+router.get('/sentimentTest/:handle', function (req, res, next) {
+  var params = { screen_name: req.params.handle, tweet_mode: 'extended', count: 10 };
   client.get('statuses/user_timeline', params, function (error, tweets, response) {
   
       if (error) {
           console.log("twitterError", error);
           return;
       };
-  
-      console.log("Donald Trumps most recent tweet:\n" )
-      console.log(tweets[0].full_text);
-  
-      text = tweets[0].full_text;
-  
-  
-      var response = function (emotions) {
 
-        res.json({emotions: emotions, tweet: text});
+      // res.json(tweets);
 
-/*           console.log(line);
-          for (var key in res) {
-              console.log(key + ": " + Math.floor(res[key] * 100) + "%");
-          }; */
-      };
-      var logError = function (err) { console.log(err); }
-  
-      indico.emotion(text)
-          .then(response)
-          .catch(logError);
+      let tweetsTextArray = [];
+
+      tweets.forEach(element => {
+        tweetsTextArray.push(element.full_text);
+      });
+
+
+      indico.emotion(tweetsTextArray)
+          .then(function (emotions) {
+            let tweetsObj = []
+
+            emotions.forEach(function (element, index) {
+              tweetsObj.push({ text: tweetsTextArray[index], emotions: element});
+            });
+
+            res.json(tweetsObj);
+          })
+          .catch(function (err) { console.log(err); });
   
   });
 });
