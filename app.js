f6de8fa6e2ecb2b9b2b3772f3eeea6c697fae2dd
JustMatt <MatthewBertrandt@gmail.com>
2018-08-16 21:03:00 -0700

    fixed bugs related with passport.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/user_details-api-routes');
 var authRouter = require('./routes/auth-routes.js');
+var passport = require('./config/passport.js')
 
 var app = express();
 var db = require("./models");
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,11 +1,19 @@
+var express = require('express');
+var db = require("../models");
+var router = express.Router();
 const passport = require("passport");
-module.exports = function(app) {
-    app.get('/auth/login', passport.authenticate("google", 
+//all these routes will ALWAYS start with /auth/
+
+router.get('/login/google', passport.authenticate("google",
     {
         scope: ['profile', 'email']
     }));
 
-    app.get('/auth/google/redirect', passport.authenticate("google"), (req,res) => {
-        
-    });
-};
+router.get('/auth/google/redirect', passport.authenticate("google"), (req, res) => {
+
+});;
+
+router.get('/login', function (req, res) {
+    res.send('help');
+});
+module.exports = router;
