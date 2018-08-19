9189efb9915856570d8fd895bf449ca45a186b52
JustMatt <MatthewBertrandt@gmail.com>
2018-08-19 12:49:07 -0700

    some kind of merge issue fixed here

--- app.js
+++ app.js
@@ -46,12 +46,12 @@ require('./routes/handles-api-routes.js')(app);
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -62,7 +62,7 @@ app.use(function(err, req, res, next) {
 });
 
 //Start up the app and sequelize
-db.sequelize.sync({force: true}).then(function () {
+db.sequelize.sync({ force: true }).then(function () {
   app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
